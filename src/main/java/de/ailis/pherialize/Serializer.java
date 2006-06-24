8eba12559c1c37008c87d377ac5bfcfdb953def7
Klaus Reimer <k@ailis.de>
2006-06-24 12:38:49 +0000

    Initial upload

---
+++ src/main/java/de/ailis/pherialize/Serializer.java
@@ -0,0 +1,342 @@
+/*
+ * $Id$
+ * Copyright (C) 2006 Klaus Reimer <k@ailis.de>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+package de.ailis.pherialize;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import de.ailis.pherialize.exceptions.SerializeException;
+
+
+/**
+ * Pherialize
+ * 
+ * @author Klaus Reimer (k.reimer@iplabs.de)
+ * @version $Revision$
+ */
+
+public class Serializer
+{
+    /** The string buffer used for serializing */
+    private StringBuffer buffer;
+    
+    /** The object history for resolving references */
+    private List history;
+
+
+    /**
+     * Constructor
+     */
+
+    private Serializer()
+    {
+        super();
+        this.buffer = new StringBuffer();
+        this.history = new ArrayList();
+    }
+
+
+    /**
+     * @see java.lang.Object#toString()
+     */
+
+    public String toString()
+    {
+        return this.buffer.toString();
+    }
+
+
+    /**
+     * Returns the serialized represetation of the specified object.
+     * 
+     * @param object
+     *            The object to serialize
+     * @return The serialized representation of the object
+     */
+
+    public static String serialize(Object object)
+    {
+        Serializer pherialize;
+
+        pherialize = new Serializer();
+        pherialize.serializeObject(object);
+        return pherialize.toString();
+    }
+
+
+    /**
+     * This method is used internally for recursively scanning the object while
+     * serializing.
+     * 
+     * @param object
+     *            The object to serialize
+     */
+
+    private void serializeObject(Object object)
+    {
+        if (serializeReference(object)) return;
+        
+        if (object == null)
+        {
+            serializeNull();
+        }
+        else if (object instanceof String)
+        {
+            serializeString((String) object);
+        }
+        else if (object instanceof Integer)
+        {
+            serializeInteger(((Integer) object).intValue());
+        }
+        else if (object instanceof Short)
+        {
+            serializeInteger(((Short) object).intValue());
+        }
+        else if (object instanceof Byte)
+        {
+            serializeInteger(((Byte) object).intValue());
+        }
+        else if (object instanceof Long)
+        {
+            serializeLong(((Long) object).longValue());
+        }
+        else if (object instanceof Double)
+        {
+            serializeDouble(((Double) object).doubleValue());
+        }
+        else if (object instanceof Float)
+        {
+            serializeDouble(((Float) object).doubleValue());
+        }
+        else if (object instanceof Boolean)
+        {
+            serializeBoolean((Boolean) object);
+        }
+        else if (object instanceof Collection)
+        {
+            serializeCollection((Collection) object);
+        }
+        else if (object instanceof Map)
+        {
+            serializeMap((Map) object);
+        }
+        else
+        {
+            throw new SerializeException("Unable how to serialize "
+                + object.getClass().getName());
+        }
+        
+        this.history.add(object);
+    }
+    
+    
+    /**
+     * Tries to serialize a reference if the specified object was already
+     * serialized. It returns true in this case. If the object was not
+     * serialized before then false is returned.
+     *
+     * @param object The object to serialize
+     * @return If a reference was serialized or not
+     */
+    
+    private boolean serializeReference(Object object)
+    {
+        Iterator iterator;
+        int index;
+        boolean isReference;
+       
+        iterator = this.history.iterator();
+        index = 0;
+        isReference = false;
+        while (iterator.hasNext())
+        {
+            if (iterator.next() == object)
+            {
+                this.buffer.append("R:");
+                this.buffer.append(index);
+                this.buffer.append(';');
+                isReference = true;
+                break;
+            }
+            index++;
+        }
+        return isReference;
+    }
+
+
+    /**
+     * Serializes the specified string and appends it to the serialization
+     * buffer.
+     * 
+     * @param string
+     *            The string to serialize
+     */
+
+    private void serializeString(String string)
+    {
+        this.buffer.append("s:");
+        this.buffer.append(string.length());
+        this.buffer.append(":\"");
+        this.buffer.append(string);
+        this.buffer.append("\";");
+    }
+
+
+    /**
+     * Adds a serialized NULL to the serialization buffer.
+     */
+
+    private void serializeNull()
+    {
+        this.buffer.append("N;");
+    }
+
+
+    /**
+     * Serializes the specified integer number and appends it to the
+     * serialization buffer.
+     * 
+     * @param number
+     *            The integer number to serialize
+     */
+
+    private void serializeInteger(int number)
+    {
+        this.buffer.append("i:");
+        this.buffer.append(number);
+        this.buffer.append(';');
+    }
+
+
+    /**
+     * Serializes the specified lonf number and appends it to the serialization
+     * buffer.
+     * 
+     * @param number
+     *            The lonf number to serialize
+     */
+
+    private void serializeLong(long number)
+    {
+        if ((number >= Integer.MIN_VALUE) && (number <= Integer.MAX_VALUE))
+        {
+            this.buffer.append("i:");
+        }
+        else
+        {
+            this.buffer.append("d:");
+        }
+        this.buffer.append(number);
+        this.buffer.append(';');
+    }
+
+
+    /**
+     * Serializes the specfied double number and appends it to the serialization
+     * buffer.
+     * 
+     * @param number
+     *            The number to serialize
+     */
+
+    private void serializeDouble(double number)
+    {
+        this.buffer.append("d:");
+        this.buffer.append(number);
+        this.buffer.append(';');
+    }
+
+
+    /**
+     * Serializes the specfied boolean and appends it to the serialization
+     * buffer.
+     * 
+     * @param value
+     *            The value to serialize
+     */
+
+    private void serializeBoolean(Boolean value)
+    {
+        this.buffer.append("b:");
+        this.buffer.append(value.booleanValue() ? 1 : 0);
+        this.buffer.append(';');
+    }
+
+
+    /**
+     * Serializes the specfied collection and appends it to the serialization
+     * buffer.
+     * 
+     * @param collection
+     *            The collection to serialize
+     */
+
+    private void serializeCollection(Collection collection)
+    {
+        Iterator iterator;
+        int index;
+
+        this.buffer.append("a:");
+        this.buffer.append(collection.size());
+        this.buffer.append(":{");
+        iterator = collection.iterator();
+        index = 0;
+        while (iterator.hasNext())
+        {
+            serializeObject(Integer.valueOf(index));
+            serializeObject(iterator.next());
+            index++;
+        }
+        this.buffer.append('}');
+    }
+
+
+    /**
+     * Serializes the specfied map and appends it to the serialization buffer.
+     * 
+     * @param map
+     *            The map to serialize
+     */
+
+    private void serializeMap(Map map)
+    {
+        Iterator iterator;
+        Object key;
+
+        this.buffer.append("a:");
+        this.buffer.append(map.size());
+        this.buffer.append(":{");
+        iterator = map.keySet().iterator();
+        while (iterator.hasNext())
+        {
+            key = iterator.next();
+            serializeObject(key);
+            serializeObject(map.get(key));
+        }
+        this.buffer.append('}');
+    }
+}
---
+++ src/test/java/de/ailis/pherialize/SerializerTest.java
@@ -0,0 +1,236 @@
+/*
+ * $Id$
+ * Copyright (c) 2006 Klaus Reimer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to 
+ * deal in the Software without restriction, including without limitation the 
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+ * sell copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ * IN THE SOFTWARE. 
+ */
+
+package de.ailis.pherialize;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+
+/**
+ * Test class for Serializer
+ * 
+ * @author Klaus Reimer (k@ailis.de)
+ * @version $Revision$
+ */
+
+public class SerializerTest extends TestCase
+{
+    /**
+     * Returns the test suite.
+     * 
+     * @return The test suite
+     */
+
+    public static Test suite()
+    {
+        return new TestSuite(SerializerTest.class);
+    }
+
+
+    /**
+     * Tests serializing a string
+     */
+
+    public void testSerializeString()
+    {
+        String test;
+
+        test = "Test";
+        assertEquals("s:4:\"Test\";", Serializer.serialize(test));
+    }
+
+
+    /**
+     * Tests serializing an integer value.
+     */
+
+    public void testSerializeInteger()
+    {
+        assertEquals("i:0;", Serializer.serialize(Integer.valueOf(0)));
+        assertEquals("i:-1;", Serializer.serialize(Integer.valueOf(-1)));
+        assertEquals("i:2147483647;", Serializer.serialize(Integer
+            .valueOf(2147483647)));
+        assertEquals("i:-2147483648;", Serializer.serialize(Integer
+            .valueOf(-2147483648)));
+    }
+
+
+    /**
+     * Tests serializing a short value.
+     */
+
+    public void testSerializeShort()
+    {
+        assertEquals("i:0;", Serializer.serialize(Short.valueOf((short) 0)));
+        assertEquals("i:-1;", Serializer.serialize(Short.valueOf((short) -1)));
+        assertEquals("i:32767;", Serializer.serialize(Short
+            .valueOf((short) 32767)));
+        assertEquals("i:-32768;", Serializer.serialize(Short
+            .valueOf((short) -32768)));
+    }
+
+
+    /**
+     * Tests serializing a byte value.
+     */
+
+    public void testSerializeByte()
+    {
+        assertEquals("i:0;", Serializer.serialize(Byte.valueOf((byte) 0)));
+        assertEquals("i:-1;", Serializer.serialize(Byte.valueOf((byte) -1)));
+        assertEquals("i:127;", Serializer.serialize(Byte.valueOf((byte) 127)));
+        assertEquals("i:-128;", Serializer.serialize(Byte.valueOf((byte) -128)));
+    }
+
+
+    /**
+     * Tests serializing a long value.
+     */
+
+    public void testSerializeLong()
+    {
+        assertEquals("i:0;", Serializer.serialize(Long.valueOf(0)));
+        assertEquals("i:-1;", Serializer.serialize(Long.valueOf(-1)));
+        assertEquals("i:2147483647;", Serializer.serialize(Long
+            .valueOf(2147483647)));
+        assertEquals("i:-2147483648;", Serializer.serialize(Long
+            .valueOf(-2147483648)));
+        assertEquals("d:2147483648;", Serializer.serialize(Long
+            .valueOf(2147483648L)));
+        assertEquals("d:-2147483649;", Serializer.serialize(Long
+            .valueOf(-2147483649L)));
+    }
+
+
+    /**
+     * Tests serializing a double value.
+     */
+
+    public void testSerializeDouble()
+    {
+        assertEquals("d:0.0;", Serializer.serialize(Double.valueOf(0)));
+        assertEquals("d:-1.0;", Serializer.serialize(Double.valueOf(-1)));
+        assertEquals("d:-10.1;", Serializer.serialize(Double.valueOf(-10.1)));
+        assertEquals("d:545.159122;", Serializer.serialize(Double
+            .valueOf(545.159122)));
+    }
+
+
+    /**
+     * Tests serializing a double value.
+     */
+
+    public void testSerializeFloat()
+    {
+        assertEquals("d:0.0;", Serializer.serialize(Float.valueOf(0)));
+        assertEquals("d:-1.0;", Serializer.serialize(Float.valueOf(-1)));
+        assertEquals("d:-10.100000381469727;", Serializer.serialize(Float
+            .valueOf((float) -10.100000381469727)));
+        assertEquals("d:545.1591186523438;", Serializer.serialize(Float
+            .valueOf((float) 545.1591186523438)));
+    }
+
+
+    /**
+     * Tests serializing a boolean value.
+     */
+
+    public void testSerializeBoolean()
+    {
+        assertEquals("b:0;", Serializer.serialize(Boolean.FALSE));
+        assertEquals("b:1;", Serializer.serialize(Boolean.TRUE));
+    }
+
+
+    /**
+     * Tests serializing a null value.
+     */
+
+    public void testSerializeNull()
+    {
+        assertEquals("N;", Serializer.serialize(null));
+    }
+
+
+    /**
+     * Tests serializing a collection of various values.
+     */
+
+    public void testSerializeCollection()
+    {
+        List test;
+
+        test = new ArrayList();
+        test.add("Test");
+        test.add(Integer.valueOf(12345));
+        test.add(Boolean.TRUE);
+        assertEquals("a:3:{i:0;s:4:\"Test\";i:1;i:12345;i:2;b:1;}", Serializer
+            .serialize(test));
+    }
+
+
+    /**
+     * Tests serializing a map of various values.
+     */
+
+    public void testSerializeMap()
+    {
+        Map test;
+
+        test = new HashMap();
+        test.put("key1", "Test");
+        test.put(Integer.valueOf(1), Integer.valueOf(12345));
+        test.put("key2", Boolean.TRUE);
+        assertEquals(
+            "a:3:{s:4:\"key1\";s:4:\"Test\";i:1;i:12345;s:4:\"key2\";b:1;}",
+            Serializer.serialize(test));
+    }
+
+    
+    /**
+     * Tests serializing an array with a reference in it.
+     */
+
+    public void testSerializeReference()
+    {
+        List test;
+        String test2;
+        
+        test = new ArrayList();
+        test2 = "Test 2";
+        test.add("Test 1");
+        test.add(test2);
+        test.add(new String("Test 1"));
+        test.add(test2);
+        test.add(new String("Test 1"));
+        assertEquals("a:5:{i:0;s:6:\"Test 1\";i:1;s:6:\"Test 2\";i:2;s:6:\"Test 1\";i:3;R:3;i:4;s:6:\"Test 1\";}",
+            Serializer.serialize(test));
+    }
+}
