b11a5284178fb2d53be3c7b08e677adca1ca8be9
Joshua Newman <josh@coursera.org>
2013-08-01 17:19:52 -0700

    Add unicode support to Unserializer.

--- src/test/java/de/ailis/pherialize/UnserializerTest.java
+++ src/test/java/de/ailis/pherialize/UnserializerTest.java
@@ -2,23 +2,23 @@
  * $Id$
  * Copyright (c) 2009 Klaus Reimer
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy 
- * of this software and associated documentation files (the "Software"), to 
- * deal in the Software without restriction, including without limitation the 
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
- * sell copies of the Software, and to permit persons to whom the Software is 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in 
+ *
+ * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
- * IN THE SOFTWARE. 
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
  */
 
 package de.ailis.pherialize;
@@ -30,7 +30,7 @@ import junit.framework.TestSuite;
 
 /**
  * Test class for Pherialize
- * 
+ *
  * @author Klaus Reimer (k@ailis.de)
  * @version $Revision$
  */
@@ -39,7 +39,7 @@ public class UnserializerTest extends TestCase
 {
     /**
      * Returns the test suite.
-     * 
+     *
      * @return The test suite
      */
 
@@ -59,6 +59,19 @@ public class UnserializerTest extends TestCase
 
         data = "s:4:\"Test\"";
         assertEquals("Test", Pherialize.unserialize(data).toString());
+    }
+
+
+    /**
+     * Tests unserializing a UTF-8 string
+     */
+    public void testUnserializeUnicodeString()
+    {
+        String data;
+
+        data = "s:52:\"Что такое машинное обучение?\";";
+        assertEquals("Что такое машинное обучение?",
+            Pherialize.unserialize(data).toString());
     }
 
 
